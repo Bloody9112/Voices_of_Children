ce358228ba8a564df2313d6efa34a5b62b929379
Bloody9112 <57665482+Bloody9112@users.noreply.github.com>
2023-12-05 18:46:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,3 +10,19 @@ navBtn.onclick = function () {
     document.body.classList.toggle('no-scroll')
 }
 
+
+/* News */
+
+const newsBlock1 = document.getElementById('newsBlock1');
+const newsBlock2 = document.getElementById('newsBlock2');
+const newsBlock3 = document.getElementById('newsBlock3');
+
+
+function changeBackground(element, imageUrl) {
+    element.style.backgroundImage = `url(${imageUrl})`;
+}
+
+changeBackground(newsBlock1, './../../../img/main/section_3/frame_1.jpg');
+changeBackground(newsBlock2, './../../../img/main/section_3/frame_2.jpg');
+changeBackground(newsBlock3, './../../../img/main/section_3/frame_3.jpg');
+
